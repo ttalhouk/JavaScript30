1340d1697a0be6e47093ec415abca95551481f9a
Talal Talhouk <talal@sbcglobal.net>
2016-12-29 11:42:22 -0800

    add dropdown animation with background

--- 25 - Event Capture, Propagation, Bubbling and Once/script.js
+++ 25 - Event Capture, Propagation, Bubbling and Once/script.js
@@ -2,10 +2,18 @@ const divs = document.querySelectorAll('div');
 
 function logText(e) {
   console.log(this.classList.value);
+  e.stopPropogation(); // stops bubbling returns first event only
 }
 
 divs.forEach(div => {
   return(
-    div.addEventListener('click', logText);
+    // div.addEventListener('click', logText); // bubble up
+
+    div.addEventListener('click', logText, {capture: true}); // fired on capture down
+
+    div.addEventListener('click', logText, {
+      capture: false,
+      once: true
+    }); // removes eventlistener after one trigger
   )
 })
